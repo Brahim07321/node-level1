026d44f67962ba0e66383382179e885d9eec3b16
Brahim zarid <brahimzarit1@gmail.com>
2026-03-08 22:49:19 +0000

    mongodb connt

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -13,9 +13,9 @@ const addUserRoute = require('./routes/addUser')
 
 // الاتصال بـ MongoDB localhost
 mongoose
-  .connect("mongodb://127.0.0.1:27017/all-data") // "all-data" hiya database
+  .connect("mongodb://brahimzarid7_db_user:5vowGR9ZfqyVGy0m@ac-39uzab0-shard-00-00.k8bd7sb.mongodb.net:27017,ac-39uzab0-shard-00-01.k8bd7sb.mongodb.net:27017,ac-39uzab0-shard-00-02.k8bd7sb.mongodb.net:27017/all-data?ssl=true&replicaSet=atlas-2oz14f-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
-    console.log("✅ MongoDB Connected (localhost)");
+    console.log("✅ MongoDB Connected");
     app.listen(port, () => {
       console.log(`Server running: http://localhost:${port}`);
     });
@@ -23,6 +23,7 @@ mongoose
   .catch((err) => {
     console.log("❌ MongoDB Connection Error:", err.message);
   });
+  //5vowGR9ZfqyVGy0m
 
 
   app.use(allRoutes)
